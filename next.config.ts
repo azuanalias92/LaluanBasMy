5b7c8bb3ec9ca15b844c01ac8aadc44c31517dd5
Azuan Alias <azuanalias92@gmail.com>
2025-08-17 14:18:45 +0800

    Refactor : enable back export for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export", // Disabled to enable API routes
+  output: "export",
 };
 
 export default nextConfig;
